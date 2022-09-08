dc120317053d5d49652cb9d5762b0e3ea29228bd
AdishRao <Adish.rao98@gmail.com>
2022-09-08 09:26:46 -0700

    Basic layout in single file

--- main.erl
+++ main.erl
@@ -1,27 +1,48 @@
 -module(main).
 
--export([main/1]).
+-compile(export_all).
 
 main(Args) ->
-    [K, IP | _] = Args,
+    [K, N_Nodes | Nodes ] = Args,
     Prefix =
         lists:concat(
             lists:duplicate(K, "0")),
-    io:fwrite("String : ~p ~p ~p\n", [K, IP, Prefix]),
-    find_token(Prefix).
+    io:fwrite("String : ~p ~p ~p\n", [K, Nodes, Prefix]),
+    [net_adm:ping(Node) || Node <- Nodes],
+    {Mod, Bin, File} = code:get_object_code(main),
+    rpc:multicall(code, load_binary, [Mod, File, Bin]),
 
-find_token(Prefix) ->
+    if Nodes /= [] ->
+           [spawn_many(N_Nodes, Prefix, Node) || Node <- Nodes];
+       true ->
+           ok
+    end,
+    spawn_many(N_Nodes, Prefix, node()),
+    print_coins().
+
+print_coins() ->
+    receive
+        {Hash, String, Finder} ->
+            io:fwrite("Coin ~p and Hash ~p Found by ~p\n\n", [String, Hash, Finder]),
+            print_coins()
+    end.
+
+spawn_many(0, _, _) ->
+    ok;
+spawn_many(N, Prefix, Node) ->
+    spawn(Node, main, find_token, [Prefix, self()]),
+    spawn_many(N - 1, Prefix, Node).
+
+find_token(Prefix, Parent) ->
     String = generate_random_string(),
     Hash = calculate_hash(String),
     Coin = string:find(Hash, Prefix) =:= Hash,
     if Coin == true ->
-           io:fwrite("Coin? : ~p\n", [Coin]),
-           io:fwrite("Hash : ~p\n", [Hash]),
-           io:fwrite("Final String : ~p\n", [String]);
-       Coin /= true ->
-           false
+           Parent ! {Hash, String, self()};
+       true ->
+           ok
     end,
-    find_token(Prefix).
+    find_token(Prefix, Parent).
 
 calculate_hash(String) ->
     Hash =
@@ -33,8 +54,7 @@ calculate_hash(String) ->
 generate_random_string() ->
     Base64_string =
         base64:encode(
-            crypto:strong_rand_bytes(24)),
+            crypto:strong_rand_bytes(72)),
     Gen_string = binary_to_list(Base64_string),
     Final_string = string:join(["adishsomeshwarao", Gen_string], ";"),
     Final_string.
-

a9bf388654f86b9cd407b5fa4627f16227d3bf7e
AdishRao <Adish.rao98@gmail.com>
2022-09-12 15:57:06 -0400

    Refactored code

--- main.erl
+++ main.erl
@@ -10,14 +10,13 @@ main(Args) ->
     [net_adm:ping(Node) || Node <- Nodes],
     {Mod, Bin, File} = code:get_object_code(main),
     rpc:multicall(code, load_binary, [Mod, File, Bin]),
-    Coins_found = 0,
     if Nodes /= [] ->
            [spawn_many(N_Nodes, Prefix, Node) || Node <- Nodes];
        true ->
            ok
     end,
     spawn_many(N_Nodes, Prefix, node()),
-    print_coins(Coins_found).
+    print_coins(0).
 
 print_coins(100) ->
     ok;
@@ -26,14 +25,8 @@ print_coins(Coins_found) ->
     receive
         {Hash, String, Finder} ->
             io:fwrite("Coin ~p and Hash ~p Found by ~p\n\n", [String, Hash, Finder]),
-            New_coin_count = increment_coins_found(Coins_found),
-            print_coins(New_coin_count)
+            print_coins(Coins_found + 1)
     end.
-
-increment_coins_found(Coins_found) ->
-    New_coin_count = Coins_found + 1,
-    New_coin_count.
-
 
 spawn_many(0, _, _) ->
     ok;

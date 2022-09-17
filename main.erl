8c0acc2f4c9e6ab9c1df0de584341b7580524c96
AdishRao <Adish.rao98@gmail.com>
2022-09-17 17:49:26 -0400

    Modfied main so it does both client and server in same file if required.

--- main.erl
+++ main.erl
@@ -2,48 +2,67 @@
 
 -compile(export_all).
 
-main(Args) ->
-    [K, N_Nodes | Nodes] = Args,
-    Prefix =
-        lists:concat(
-            lists:duplicate(K, "0")),
-    io:fwrite("String : ~p ~p ~p\n", [K, Nodes, Prefix]),
-    [net_adm:ping(Node) || Node <- Nodes],
-    {Mod, Bin, File} = code:get_object_code(main),
-    rpc:multicall(code, load_binary, [Mod, File, Bin]),
-    if Nodes /= [] ->
-           [spawn_many(N_Nodes, Prefix, Node) || Node <- Nodes];
-       true ->
-           ok
-    end,
-    spawn_many(N_Nodes, Prefix, node()),
-    print_coins(0).
+start(Args) ->
+    [K, N_Nodes| ServerIP] = Args,
+    if ServerIP == [] ->
+        Prefix = lists:concat(lists:duplicate(K, "0")),
+        io:fwrite("String : ~p ~p\n", [K, Prefix]),
+        spawn_many(N_Nodes, Prefix, node(), self()),
+        register(server, self()),
+        statistics(runtime),
+        {Time, _} = timer:tc(bitcoin_server, bitcoin_server, [0, N_Nodes, Prefix]),
+        {_, Time_CPU_Since_Last_Call} = statistics(runtime),
+        io:fwrite("Total clock time: ~p\nToal CPU time ~p\n CPU time/ Run Time ~p\n", [Time/1000, Time_CPU_Since_Last_Call, Time_CPU_Since_Last_Call/(Time/1000)]);
+    ServerIP /= [] ->
+        net_adm:ping(ServerIP),
+        {server, ServerIP} ! {node, node()}
+    end.
 
-print_coins(100) ->
+    %bitcoin_server(0, N_Nodes, Prefix).
+
+bitcoin_server(100, _, _) ->
     ok;
-print_coins(Coins_found) ->
+bitcoin_server(Coins_found, N_Nodes, Prefix) ->
     receive
         {Hash, String, Finder} ->
             io:fwrite("Coin ~p and Hash ~p Found by ~p\n\n", [String, Hash, Finder]),
-            print_coins(Coins_found + 1)
+            Finder ! {mine},
+            bitcoin_server(Coins_found+1, N_Nodes, Prefix);
+        {node, Node} ->
+            io:fwrite("Spawning on Node: ~p\n", [Node]),
+            send_code(Node),
+            spawn(bitcoin_server, spawn_many, [N_Nodes, Prefix, Node, self()]),
+            bitcoin_server(Coins_found, N_Nodes, Prefix)
     end.
 
-spawn_many(0, _, _) ->
+send_code(Node) ->
+    {Mod, Bin, File} = code:get_object_code(bitcoin_server),
+    rpc:multicall([Node], code, load_binary, [Mod, File, Bin]),
+    ok.
+
+spawn_many(0, _, _, _) ->
     ok;
-spawn_many(N, Prefix, Node) ->
-    spawn(Node, main, find_token, [Prefix, self()]),
-    spawn_many(N - 1, Prefix, Node).
+spawn_many(N, Prefix, Node, Parent) ->
+        spawn(Node, bitcoin_server, find_token, [Prefix, Parent]),
+        spawn_many(N - 1, Prefix, Node, Parent).
+
 
 find_token(Prefix, Parent) ->
     String = generate_random_string(),
     Hash = calculate_hash(String),
     Coin = string:find(Hash, Prefix) =:= Hash,
     if Coin == true ->
-           Parent ! {Hash, String, self()};
-       true ->
-           ok
-    end,
-    find_token(Prefix, Parent).
+           Parent ! {Hash, String, self()},
+           receive
+                {mine} ->
+                    find_token(Prefix, Parent)
+            after
+                10000 ->
+                    ok
+            end;
+    true ->
+        find_token(Prefix, Parent)
+    end.
 
 calculate_hash(String) ->
     Hash =

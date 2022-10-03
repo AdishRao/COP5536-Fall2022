aeeab5213d750c0822b491bec0c51830d7aa0cd4
RaoSanjana <srao.gp@gmail.com>
2022-10-03 02:16:17 -0400

    Adding 3d grid topology(incomplete)

--- Assignment 2/main.erl
+++ Assignment 2/main.erl
@@ -46,6 +46,7 @@ spawn_many(N, Algo) ->
         PID = spawn_link(main, start_gossip, [Algo]),
         [PID] ++ spawn_many(N - 1, Algo).
 
+%---------------------------------------------SPAWN ACTORS--------------------------------------------
 % Spawn required number of actors for line topology and build line topology.
 spawn_actors(NoNodes, line, Algo) ->
     PIDS = spawn_many(NoNodes, Algo),
@@ -64,53 +65,338 @@ spawn_actors(NoNodes, grid_2d, Algo) ->
     TotalNoNodes = TotalNoNodesRoot*TotalNoNodesRoot,
     PIDS = spawn_many(TotalNoNodes, Algo),
     send_neighbors(grid_2d, PIDS, TotalNoNodesRoot, TotalNoNodes),
+    PIDS;
+
+% Spawn required number of actors for line topology and build 2d grid topology.
+spawn_actors(NoNodes, grid_3d, Algo) ->
+    EdgeLength = trunc(math:ceil(math:pow(NoNodes, 1/3))),
+    TotalNoNodes = math:pow(EdgeLength, 3),
+    PIDS = spawn_many(TotalNoNodes, Algo),
+    send_neighbors(grid_3d, PIDS, EdgeLength, TotalNoNodes),
     PIDS.
+%----------------------Sending Neighbors-----------------------------------------------------------------------
 
 %left edge
-send_node_neighbors(1, Side, N, PIDS) ->
+send_node_neighbors(1, Side, N, PIDS) -> %4
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), lists:nth(N + Side, PIDS), lists:nth(N - Side, PIDS)]}; 
 
 %right edge
-send_node_neighbors(0, Side, N, PIDS) ->
+send_node_neighbors(0, Side, N, PIDS) -> %4
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + Side, PIDS), lists:nth(N - Side, PIDS)]}; 
 
 % Send node neighbors for nodes not on the edge of the 2d plane
-send_node_neighbors(_, Side, N, PIDS) ->
+send_node_neighbors(_, Side, N, PIDS) -> %4
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + Side, PIDS), lists:nth(N + 1, PIDS), lists:nth(N - Side, PIDS)]}. 
 
 % Bottom row
-send_node_neighbors(edge, true, Side, N, 1, PIDS) ->
+send_node_neighbors(edge, true, Side, N, 1, PIDS) -> %6
     %left most node
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), lists:nth(N - Side, PIDS)]}; 
 
-send_node_neighbors(edge, true, Side, N, 0, PIDS) ->
+send_node_neighbors(edge, true, Side, N, 0, PIDS) -> %6
     %right most node
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N - Side, PIDS)]}; 
 
-send_node_neighbors(edge, true, Side, N, _, PIDS) ->
+send_node_neighbors(edge, true, Side, N, _, PIDS) -> %6
     %inner  nodes
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + 1, PIDS),lists:nth(N - Side, PIDS)]}; 
 
 %Top row
-send_node_neighbors(edge, false, Side, N, 1, PIDS) ->
+send_node_neighbors(edge, false, Side, N, 1, PIDS) -> %6
     %left most node
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), lists:nth(N + Side, PIDS)]}; 
 
-send_node_neighbors(edge, false, Side, N, 0, PIDS) ->
+send_node_neighbors(edge, false, Side, N, 0, PIDS) -> %6
     %right most node
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + Side, PIDS)]}; 
 
-send_node_neighbors(edge, false, Side, N, _, PIDS) ->
+send_node_neighbors(edge, false, Side, N, _, PIDS) -> %6
     % inner node
-    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + 1, PIDS),lists:nth(N + Side, PIDS)]}. 
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + 1, PIDS),lists:nth(N + Side, PIDS)]}.
+
+%Three dim - inner planes ----------------------------
+%left edge
+send_node_neighbors(1, Side, N, PIDS, threeDim) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+         lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)
+        ]}; 
+
+%right edge
+send_node_neighbors(0, Side, N, PIDS, threeDim) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+% Send node neighbors for nodes not on the edge of the 2d plane
+send_node_neighbors(_, Side, N, PIDS, threeDim) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N + 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]};
+
+%Three dim - first plane ----------------------------
+%left edge
+send_node_neighbors(1, Side, N, PIDS, firstPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)
+        ]}; 
+
+%right edge
+send_node_neighbors(0, Side, N, PIDS, firstPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+% Send node neighbors for nodes not on the edge of the 2d plane
+send_node_neighbors(_, Side, N, PIDS, firstPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N + 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]};
+
+%Three dim - last plane ----------------------------
+%left edge
+send_node_neighbors(1, Side, N, PIDS, lastPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) 
+        ]}; 
+
+%right edge
+send_node_neighbors(0, Side, N, PIDS, lastPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ]}; 
+
+% Send node neighbors for nodes not on the edge of the 2d plane
+send_node_neighbors(_, Side, N, PIDS, lastPlane) -> %5
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS), 
+        lists:nth(N + 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ]}.
+
+
+% Bottom row
+send_node_neighbors(edge, true, Side, N, 1, PIDS, threeDim) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS), 
+        % lists:nth(N + Side, PIDS), 
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, true, Side, N, 0, PIDS, threeDim) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, true, Side, N, _, PIDS, threeDim) -> %7
+    %inner  nodes
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+%Top row
+send_node_neighbors(edge, false, Side, N, 1, PIDS, threeDim) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, 0, PIDS, threeDim) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, _, PIDS, threeDim) -> %7
+    % inner node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ,
+        lists:nth(N + math:pow(Side, 2), PIDS)]};
+
+%Three dim - first plane ----------------------------
+
+
+% Bottom row
+send_node_neighbors(edge, true, Side, N, 1, PIDS, firstPlane) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS), 
+        lists:nth(N + math:pow(Side, 2), PIDS)
+    ]}; 
+
+send_node_neighbors(edge, true, Side, N, 0, PIDS, firstPlane) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, true, Side, N, _, PIDS, firstPlane) -> %7
+    %inner  nodes
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+%Top row
+send_node_neighbors(edge, false, Side, N, 1, PIDS, firstPlane) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, 0, PIDS, firstPlane) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, _, PIDS, firstPlane) -> %7
+    % inner node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N + Side, PIDS),
+        lists:nth(N + math:pow(Side, 2), PIDS)]};
+
+%Three dim - last plane ----------------------------
+
+
+% Bottom row
+send_node_neighbors(edge, true, Side, N, 1, PIDS, lastPlane) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS), 
+        % lists:nth(N + Side, PIDS), 
+        lists:nth(N - math:pow(Side, 2), PIDS) 
+    ]}; 
+
+send_node_neighbors(edge, true, Side, N, 0, PIDS, lastPlane) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, true, Side, N, _, PIDS, lastPlane) -> %7
+    %inner  nodes
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N - Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ]}; 
+%Top row
+send_node_neighbors(edge, false, Side, N, 1, PIDS, lastPlane) -> %7
+    %left most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N + 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, 0, PIDS, lastPlane) -> %7
+    %right most node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS)]}; 
+
+send_node_neighbors(edge, false, Side, N, _, PIDS, lastPlane) -> %7
+    % inner node
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), 
+        lists:nth(N + 1, PIDS),
+        lists:nth(N + Side, PIDS),
+        lists:nth(N - math:pow(Side, 2), PIDS) ]}.
+
+
+% First plane i.e., front most plane
+get_neighbors(grid_3d, PIDS, 1, Side, Node_no) -> %5
+    if Node_no =< Side ->
+         send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, firstPlane );
+    (Node_no + Side > (Side * Side)) ->
+        send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, firstPlane);
+    true ->
+        get_neighbors(Node_no rem Side, Side, Node_no, PIDS, firstPlane)
+    end;
+
+% Last plane 
+get_neighbors(grid_3d, PIDS, last, Side, Node_no) -> %5
+    if Node_no =< Side ->
+         send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, lastPlane);
+    (Node_no + Side > (Side * Side)) ->
+        send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, lastPlane);
+    true ->
+        get_neighbors(Node_no rem Side, Side, Node_no, PIDS, lastPlane)
+    end;
+
+
+% inner planes
+get_neighbors(grid_3d, PIDS, _, Side, Node_no) -> %5
+    if Node_no =< Side ->
+         send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, threeDim );
+    (Node_no + Side > (Side * Side)) ->
+        send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS, threeDim);
+    true ->
+        get_neighbors(Node_no rem Side, Side, Node_no, PIDS, threeDim)
+    end.
+
+% Find neighbors for full for node N.
+send_neighbors(full, PIDS, N) -> %3
+    Self = lists:nth(N, PIDS),
+    Self ! {neighbor, lists:delete(Self, PIDS)},
+    send_neighbors(full, PIDS, N-1);
+
+% Find neighbors for line for node N.
+send_neighbors(line, PIDS, 1) -> %3
+    lists:nth(1, PIDS) ! {neighbor, [lists:nth(2 , PIDS)]},
+    send_neighbors(line, PIDS, 0);
+
+send_neighbors(line, PIDS, N) -> %3
+    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + 1, PIDS)]},
+    send_neighbors(line, PIDS, N-1);
+
+% Finished sending neighbors.
+send_neighbors(_, _, 0) ->  %3
+    ok.  
 
 %Finished sending neighbors in the plane
-send_neighbors(grid_2d, _, _, 0)  ->
+send_neighbors(grid_2d, _, _, 0)  ->  %4
     ok;
 
+send_neighbors(grid_3d, _, _, 0)  -> %4
+    ok;
+
+%Find neighbors for 3D grid
+send_neighbors(grid_3d, PIDS, EdgeLength, Node_no) -> %4
+    PlaneNumber = Node_no div (EdgeLength*EdgeLength) ,
+    if PlaneNumber /= 0 ->
+        PlaneNumber = PlaneNumber + 1;
+    if PlaneNumber == EdgeLength ->
+        get_neighbors(grid_3d, PIDS, last, EdgeLength, Node_no);
+    true ->
+        get_neighbors(grid_3d, PIDS, PlaneNumber, EdgeLength, Node_no)
+    end,
+    % send_node_neighbors(grid_3d, PIDS, PlaneNumber, EdgeLength, Node_no),
+    send_neighbors(grid_3d, PIDS, EdgeLength, Node_no-1);
+
 % Find neighbors for 2D grid
-send_neighbors(grid_2d, PIDS, Side, Node_no) ->
-    io:fwrite("Inside send_neighbors, Running for node - ~p who has PID ~p\n\n", [Node_no, lists:nth(Node_no, PIDS)]),
+send_neighbors(grid_2d, PIDS, Side, Node_no) -> %4
+    % io:fwrite("Inside send_neighbors, Running for node - ~p who has PID ~p\n\n", [Node_no, lists:nth(Node_no, PIDS)]),
     if Node_no =< Side ->
          send_node_neighbors(edge, Node_no + Side > (Side * Side), Side, Node_no, Node_no rem Side, PIDS);
     (Node_no + Side > (Side * Side)) ->
@@ -120,48 +406,31 @@ send_neighbors(grid_2d, PIDS, Side, Node_no) ->
     end,
     send_neighbors(grid_2d, PIDS, Side, Node_no-1);
 
-
 % Find neighbors for line, given the last element. Will check that number of nodes is satisfied.
-send_neighbors(line, _, max, 1) ->
+send_neighbors(line, _, max, 1) -> %4
     exit('not enough nodes, at least 2');
-send_neighbors(line, PIDS, max, N) ->
+
+send_neighbors(line, PIDS, max, N) -> %4
     lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS)]},
     send_neighbors(line, PIDS, N-1);
 
 % Find neighbors for full, given the last element. Will check that number of nodes is satisfied.
-send_neighbors(full, _, max, 1) ->
+send_neighbors(full, _, max, 1) -> %4
     exit('not enough nodes, at least 2');
-send_neighbors(full, PIDS, max, N) ->
+
+send_neighbors(full, PIDS, max, N) -> %4
     Self = lists:nth(N, PIDS),
     Self ! {neighbor, lists:delete(Self, PIDS)},
     send_neighbors(full, PIDS, N-1).
 
-
-
-% Finished sending neighbors.
-send_neighbors(_, _, 0) ->
-    ok;
-
-% Find neighbors for full for node N.
-send_neighbors(full, PIDS, N) ->
-    Self = lists:nth(N, PIDS),
-    Self ! {neighbor, lists:delete(Self, PIDS)},
-    send_neighbors(full, PIDS, N-1);
-
-% Find neighbors for line for node N.
-send_neighbors(line, PIDS, 1) ->
-    lists:nth(1, PIDS) ! {neighbor, [lists:nth(2 , PIDS)]},
-    send_neighbors(line, PIDS, 0);
-send_neighbors(line, PIDS, N) ->
-    lists:nth(N, PIDS) ! {neighbor, [lists:nth(N - 1, PIDS), lists:nth(N + 1, PIDS)]},
-    send_neighbors(line, PIDS, N-1).
-
+%--------------------------------GOSSIP and PUSH SUM------------------------------------------------------------------
 % Actor waits for its neighbors.
 start_gossip(Algo) ->
     receive
         {neighbor, MyNeighbors} -> 
             gossip(MyNeighbors, Algo)
     end.
+
 
 % Run gossip algo.
 gossip(MyNeighbors, gossip) ->

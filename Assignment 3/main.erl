42f417893a13825af83302fc33d3a145bb63b1e2
AdishRao <Adish.rao98@gmail.com>
2022-10-17 02:36:53 -0400

    Building basic layout

--- Assignment 3/main.erl
+++ Assignment 3/main.erl
@@ -7,6 +7,37 @@
 start(numNodes, numRequests) ->
     ok.
 
+
+
+%n.find successor(id)
+find_successor(Id, ReqN, SelfN, Successor, Finger) ->
+    Bool = (Id > SelfN) and (Id =< Successor),
+    if Bool ->
+        ReqN ! {Successor};
+    true ->
+        NewN = closest_preceding_node(Id, SelfN, Finger),
+        NewN ! {findsuccessor, Id, ReqN}
+    end.
+
+closest_preceding_node(_, _, _) ->
+  ok.
+
+%n.create() n is calling create
+create(N) ->
+    Predecessor = nil,
+    Successor = N,
+    {Predecessor, Successor}.
+
+%n.join(n′ ) asking node n to join n'
+join(N, KnownNode) ->
+    Predecessor = nil,
+    KnownNode ! {findsuccessor, N, self()},
+    receive
+        {Successor} ->
+            ok
+    end,
+    {Predecessor, Successor}.
+
 hash_data(Data, number) ->
     StringData = integer_to_list(Data),
     HashBytes = crypto:hash(sha, unicode:characters_to_binary(StringData)),

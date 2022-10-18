51c3d31a719c359612383a367ac16af92af115d0
AdishRao <Adish.rao98@gmail.com>
2022-10-18 01:16:07 -0400

    Added first 3 functions, last 3 left. Main loop logic also left.

--- Assignment 3/main.erl
+++ Assignment 3/main.erl
@@ -7,22 +7,35 @@
 start(numNodes, numRequests) ->
     ok.
 
-
-
 %n.find successor(id)
+% Finger is a list of {Id, PID} tuples
 find_successor(Id, ReqN, SelfN, Successor, Finger) ->
-    Bool = (Id > SelfN) and (Id =< Successor),
+    {SuccessorID, SuccessorPID} = Successor,
+    {SelfID, SelfPID} = SelfN,
+    Bool = (Id > SelfID) and (Id =< SuccessorID),
+    io:fwrite("Bool insidde find_successor ~p for variables ~p ~p ~p", [Bool, Id, SelfID, SuccessorID]),
     if Bool ->
         ReqN ! {Successor};
     true ->
-        NewN = closest_preceding_node(Id, SelfN, Finger),
+        NewN = closest_preceding_node(Id, SelfID, Finger, ?M),
         NewN ! {findsuccessor, Id, ReqN}
     end.
 
-closest_preceding_node(_, _, _) ->
-  ok.
+%n.closest preceding node(id)
+% Finger is a list of {Id, PID} tuples
+closest_preceding_node(_, SelfID, _, 0) ->
+    SelfID;
+closest_preceding_node(Id, SelfID, Finger, I) ->
+    {FigerID, FingerPID} = lists:nth(I, Finger),
+    Bool = (FigerID > SelfID) and (FigerID < Id),
+    if Bool ->
+        FingerPID;
+    true ->
+        closest_preceding_node(Id, SelfID, Finger, I-1)
+    end.
 
 %n.create() n is calling create
+%Assume N is a tuple {ID, PID}
 create(N) ->
     Predecessor = nil,
     Successor = N,
@@ -31,12 +44,38 @@ create(N) ->
 %n.join(n′ ) asking node n to join n'
 join(N, KnownNode) ->
     Predecessor = nil,
-    KnownNode ! {findsuccessor, N, self()},
+    {NodeID, NodePID} = N,
+    KnownNode ! {findsuccessor, NodeID, NodePID},
     receive
         {Successor} ->
             ok
     end,
+    %Successor also a tuple {ID, PID}
     {Predecessor, Successor}.
+
+
+%n.stabilize()
+%N is a tuple of {ID, PID}
+stabilize(N, Successor) ->
+    {SuccessorID, SuccessorPID} = Successor,
+    {NodeID, NodePID} = N,
+    Successor ! {predecessor},
+    receive
+        {X} -> %successor.predecessor
+            ok
+    end,
+    {XId, XPID} = X,
+    Bool = (XId > NodeID) and (XId < SuccessorID),
+    if Bool ->
+        NewSuccessor = X;
+    true ->
+        NewSuccessor = Successor
+    end,
+    notify(NewSuccessor, N), % successor.notify(n);
+    NewSuccessor.
+
+notify(_, _) ->
+    ok.
 
 hash_data(Data, number) ->
     StringData = integer_to_list(Data),

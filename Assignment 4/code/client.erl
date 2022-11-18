204c70e6d50a049592208e7607a87a70565ea311
AdishRao <Adish.rao98@gmail.com>
2022-11-18 11:48:44 -0500

    Formatted strings with new line characters, and number inputs for promopts

--- Assignment 4/code/client.erl
+++ Assignment 4/code/client.erl
@@ -1,32 +1,29 @@
 -module(client).
 
-% ignore
 -compile(export_all).
-% -export([start/0, start_listening/0, start_tweeting/0, register/0, login/0, tweet/1, query/1, retweet/1, subscribe/1]).
 
 start(Server_node) ->
     net_adm:ping(Server_node),
-    New_or_Current_User = string:chomp(io:get_line("Login or Register: ")),
-    if New_or_Current_User == "login" ->
-        io:fwrite("Inside login if\n"),
+    New_or_Current_User = string:chomp(io:get_line("1. Login\n2. Register\n")),
+    if New_or_Current_User == "1" ->
+        io:fwrite("Enter Login Details\n"),
         login(Server_node);
     true ->
-        io:fwrite("Inside else\n"),
+        io:fwrite("Enter Register Details\n"),
         register(Server_node)  
     end,
     start_listening(Server_node).
-    
 
 start_listening() ->
     receive
         {tweet_response, error} ->
-            io:fwrite("Error tweeting");
+            io:fwrite("Error tweeting\n");
         {tweet_response, ok} ->
-            io:fwrite("Tweet successful");
+            io:fwrite("Tweet successful\n");
         {query_response, Tweet, TweetId, Tweeter} ->
-            io:fwrite("Query Response | Tweet\t~p\n, Tweeter \t~p\n, TweetID\t~p\n", [Tweet, Tweeter, TweetId]);
+            io:fwrite("Query Response \nTweet\t~p \nTweeter \t~p \nTweetID\t~p\n", [Tweet, Tweeter, TweetId]);
         {new_tweet, Tweet, Tweeter, TweetId} ->
-            io:fwrite("New Tweet | Tweet\t~p\n, Tweeter \t~p\n, TweetID\t~p\n", [Tweet, Tweeter, TweetId])
+            io:fwrite("New Tweet \nTweet\t~p \nTweeter \t~p \nTweetID\t~p\n", [Tweet, Tweeter, TweetId])
     end,
     start_listening().
 
@@ -43,45 +40,45 @@ start_listening(Server_node) ->
             spawn_link(client, start_tweeting, [Server_node, Username]),
             start_listening();
         {login_response, error} ->
-            io:fwrite("Login Error"),
+            io:fwrite("Login Error\n"),
             start(Server_node);
         {register_response, error} ->
-            io:fwrite("Registration error"),
+            io:fwrite("Registration error\n"),
             start(Server_node)
     end.
 
 start_tweeting(Server_node, Username) ->
-    Operation = string:chomp(io:get_line("You can now tweet, query, subscribe or retweet. What would you like to do?\n")),
+    Operation = string:chomp(io:get_line("1.tweet \n2.query \n3.subscribe \n4.retweet\n")),
     case Operation of 
-        "tweet" -> tweet(Server_node, Username);
-        "subscribe" -> subscribe(Server_node, Username);
-        "query" -> query(Server_node, Username);
-        "retweet" -> retweet(Server_node, Username)
+        "1" -> tweet(Server_node, Username);
+        "2" -> query(Server_node, Username);
+        "3" -> subscribe(Server_node, Username);
+        "4" -> retweet(Server_node, Username);
+        _ -> ok
     end,
     start_tweeting(Server_node, Username).
 
 register(Server_node) ->
-    Username = string:chomp(io:get_line("Enter Username: ")),
-    Password = string:chomp(io:get_line("Enter Password: ")),
+    Username = string:chomp(io:get_line("Enter Username\n")),
+    Password = string:chomp(io:get_line("Enter Password\n")),
     {server, Server_node} ! {client_register, self(), Username, Password}.
     
 login(Server_node)->
-    Username = string:chomp(io:get_line("Enter Username: ")),
-    Password = string:chomp(io:get_line("Enter Password: ")),
-    io:fwrite("Username: ~p and Password ~p\n",[Username, Password]),
+    Username = string:chomp(io:get_line("Enter Username\n")),
+    Password = string:chomp(io:get_line("Enter Password\n")),
     {server, Server_node}  ! {client_login, self(), Username, Password}.
 
 tweet(Server_node, Username) ->
-    Tweet = string:chomp(io:get_line("What would you like to say? ")),
+    Tweet = string:chomp(io:get_line("What would you like to say?\n")),
     {server, Server_node}  ! {client_tweet, Username, Tweet}.
 
 subscribe(Server_node, Username) ->
-    New_subscription_raw = string:chomp(io:get_line("What are you subscribing to? ")),
+    New_subscription_raw = string:chomp(io:get_line("What are you subscribing to?\n")),
     New_subscription = process_string(New_subscription_raw),
     {server, Server_node}  ! {client_subscribe_to, Username, New_subscription}.
 
 query(Server_node, Username)->
-    Query_raw = string:chomp(io:get_line("What/Whom do you want to know more about? ")),
+    Query_raw = string:chomp(io:get_line("What/Whom do you want to know more about?\n")),
     Query = process_string(Query_raw),
     {server, Server_node}  ! {client_query, Username, Query}.
 
@@ -100,5 +97,5 @@ process_string(String) ->
     ReturnString.
 
 retweet(Server_node, Username)->
-    {ok, TweetId} = io:read("Enter the tweet id you'd like to retweet"),
+    {ok, TweetId} = io:read("Enter the tweet id you'd like to retweet.\n"),
     {server, Server_node} ! {client_retweet, Username, TweetId}.
